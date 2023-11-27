f1603fb6f857e1c95f869688a5a64f0f342aa8f5
KhanhHuyen2810 <145785839+KhanhHuyen2810@users.noreply.github.com>
2023-11-27 16:32:04 +0700

    Update menuController.java

--- src/main/java/com/example/baitaplonoop/menuController.java
+++ src/main/java/com/example/baitaplonoop/menuController.java
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit;
 
 import java.io.IOException;
 
-public class menuController implements Initializable {
+public class menuController {
 
     @FXML
     private Button gameButton;
@@ -29,51 +29,22 @@ public class menuController implements Initializable {
     private Button dictionaryButton;
 
     @FXML
-    private ImageView ship;
-
-    TranslateTransition transition = new TranslateTransition();
-
-    @FXML
     public void showDictionary(MouseEvent event) throws IOException {
-        try {
-            Thread.sleep(1100);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
         Stage stage = (Stage) dictionaryButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dictionary.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Dictionary");
         stage.setScene(scene);
-        transition.setToX(138);
-        transition.setToY(219);
-        transition.setDuration(Duration.millis(1000));
-        transition.play();
         DictionaryManagement dictionaryManagement = new DictionaryManagement();
         dictionaryManagement.insertFromFile();
     }
 
     @FXML
     public void showGame(MouseEvent event) throws IOException{
-        try {
-            Thread.sleep(2100);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
         Stage stage = (Stage) dictionaryButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("game.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Game");
         stage.setScene(scene);
-        transition.setToX(395);
-        transition.setToY(197);
-        transition.setDuration(Duration.millis(2000));
-        transition.play();
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        TranslateTransition transition = new TranslateTransition();
-        transition.setNode(ship);
     }
 }
